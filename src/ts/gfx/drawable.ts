195d0312fbcf64afd5718936e445ea458d0c710e
Xander Vedder <xander.vedd@gmail.com>
2020-12-24 12:21:44 +0100

    Add clearAll method in Drawable

--- src/ts/gfx/drawable.ts
+++ src/ts/gfx/drawable.ts
@@ -11,6 +11,10 @@ export abstract class Drawable {
     return Drawable._drawables;
   }
 
+  public static clearAll() {
+    Drawable._drawables = [];
+  }
+
   abstract draw(ctx: CanvasRenderingContext2D): void;
   abstract onClick(event: Position): void;
   abstract pointInArea(x: number, y: number): boolean;
