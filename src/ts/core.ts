596a9e4adc694865c68e515eed21ac810fcfd523
Xander Vedder <xander.vedd@gmail.com>
2020-12-24 12:25:13 +0100

    Update imports of core.ts
    closes #1

--- src/ts/core.ts
+++ src/ts/core.ts
@@ -1,9 +1,9 @@
 import { Circuit } from "./circuit.js";
-import { NAND } from "./gate/nand.js";
+import { Nand } from "./gfx/gate/nand.js";
 
-new NAND(250, 250);
-new NAND(250, 550);
-new NAND(750, 375);
+new Nand(250, 250);
+new Nand(250, 550);
+new Nand(750, 375);
 
 const circuit = new Circuit();
 circuit.start();
