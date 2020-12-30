9ca97ddf7998337a68559d6fdfd95d2de7dd7a1d
Xander Vedder <xander.vedd@gmail.com>
2020-12-30 17:57:02 +0100

    Add more utillity position methods

--- src/ts/gfx/util/position.ts
+++ src/ts/gfx/util/position.ts
@@ -1,4 +1,15 @@
-export const getPosition = (event: MouseEvent) => ({
-  x: event.offsetX,
-  y: event.offsetY,
-});
+import { Position } from "../../type/types";
+
+export function p(event: MouseEvent): Position {
+  return {
+    x: event.offsetX,
+    y: event.offsetY,
+  }
+}
+
+export function pXY(x: number, y: number): Position {
+  return {
+    x: x,
+    y: y,
+  }
+}
