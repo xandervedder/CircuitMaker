445437fb0c7fdbe89264bd72cc239c4ff8fd84ea
Xander Vedder <xander.vedd@gmail.com>
2021-01-02 20:11:25 +0100

    Update core.ts
    Updates some parts of the UI.
    
    closes #7

--- src/ts/core.ts
+++ src/ts/core.ts
@@ -1,19 +1,53 @@
 import { LayerType, LayerManager } from "./layer/layer-manager";
+import { $, all } from "./ui/dom";
+
+const version = "v0.0";
+document.title = `CircuitMaker - ${version}`;
 
 const manager = new LayerManager();
 manager.switchActiveLayer(LayerType.Preview);
 
-const tooltip = document.querySelector("#tooltip");
+const toastElement = $("#toast");
+const toastTextElement = $("#toastText");
 
-document.querySelector("#switchLayer").addEventListener("click", () => {
-  if (manager.activeLayer === LayerType.Draw) {
-    manager.switchActiveLayer(LayerType.Preview);
-    tooltip.textContent = "Current layer: Preview";
-  } else {
-    manager.transferDrawables();
-    manager.switchActiveLayer(LayerType.Draw);
-    tooltip.textContent = "Current layer: Draw"
-  }
+const toolbox = $("#toolbox");
+const toolboxButton = $("#toolboxButton");
+toolboxButton.addEventListener("click", () => {
+  toolbox.classList.toggle("toolbox--open");
 });
 
-document.querySelector("#cable").addEventListener("click", () => manager.transferDrawables())
+const tools = [...all("#gates .toolbox__button"), ...all("#placement .toolbox__button"), ...all("#nodes .toolbox__button")];
+tools.forEach(tool => {
+  tool.addEventListener("click", () => {
+    tools.forEach(_tool => _tool.classList.remove("toolbox__button--toggled"));
+    tool.classList.add("toolbox__button--toggled");
+  });
+})
+
+const layerTools = all("#layers .toolbox__button");
+layerTools.forEach(tool => {
+  tool.addEventListener("click", () => {
+    layerTools.forEach(_tool => _tool.classList.remove("toolbox__button--toggled"));
+    tool.classList.add("toolbox__button--toggled");
+
+    if (tool.id === "preview") {
+      toastTextElement.textContent = "Preview";
+      manager.switchActiveLayer(LayerType.Preview);
+    } else {
+      toastTextElement.textContent = "Draw"
+      manager.transferDrawables();
+      manager.switchActiveLayer(LayerType.Draw);
+    }
+
+    showToast();
+  });
+})
+
+function showToast() {
+  toastElement.classList.remove("toast--hide");
+  setTimeout(() => {
+    toastElement.classList.add("toast--hide");
+  }, 1500);
+}
+
+showToast();
